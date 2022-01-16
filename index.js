976021bd8bc31eee3d899aca48e784ecee9896fb
Emmanuel Tamburini <e.tamburini@bucares.com>
2022-01-16 08:28:04 -0400

    Third version has been done: koa logger has been added

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const client = require("./repository");
-
 const koa = require('koa');
+let logger = require('koa-logger');
+
 const app = new koa();
 const bodyParser = require('koa-bodyparser');
 const moviesRouter = require('./controller/Movies.controller')
+
+app.use(logger((str, args) => {
+    console.log(str, args);
+}));
 
 app.use(bodyParser());
 
